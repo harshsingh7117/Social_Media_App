917a90583881eddd1d97ff5b17751689fbd72fb9
harshsingh7117 <harshsingh7117@gmail.com>
2024-06-13 21:22:03 +0530

    .

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,27 +1,8 @@
 import express from "express";
-import { body } from "express-validator";
-import { register, login } from "../controllers/auth.js";
+import { login } from "../controllers/auth.js";
 
 const router = express.Router();
 
-router.post(
-  "/register",
-  [
-    body("firstName").notEmpty().withMessage("First name is required"),
-    body("lastName").notEmpty().withMessage("Last name is required"),
-    body("email").isEmail().withMessage("Valid email is required"),
-    body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long"),
-  ],
-  register
-);
-
-router.post(
-  "/login",
-  [
-    body("email").isEmail().withMessage("Valid email is required"),
-    body("password").notEmpty().withMessage("Password is required"),
-  ],
-  login
-);
+router.post("/login", login);
 
 export default router;
